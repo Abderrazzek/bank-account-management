318ef7bc27731c7f365f87175a31de87e210f2dc
YOUNSI <abderrazek.younsi@cgi.com>
2024-05-10 18:37:47 +0200

    :fire: remove uneeded axios instance since we are using json server

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -5,50 +5,46 @@ type ApiResponse<T> = Promise<AxiosResponse<T>>;
 
 const api: string = "http://localhost:3001/";
 
-// Axios instance used to pass the bearer token into the BE
-const axiosInstance = axios.create({
-  baseURL: api,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
-
-// Axios instance used in non-auth mode, for full-screen mode
-const publicAxiosInstance = axios.create({
-  baseURL: api,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
-
 /**
  * Used to call GET HTTP requests
  */
-export const getAPI = <T>(url: string, config?: AxiosRequestConfig): ApiResponse<T> => {
-  return publicAxiosInstance.get<T>(url, config);
+export const getAPI = <T>(
+  url: string,
+  config?: AxiosRequestConfig
+): ApiResponse<T> => {
+  return axios.get<T>(api + url, config);
 };
 
 /**
  * Used to call POST HTTP requests
  */
-export const postAPI = <T>(url: string, data: any, config?: AxiosRequestConfig): ApiResponse<T> => {
-  return axiosInstance.post<T>(url, data, config);
+export const postAPI = <T>(
+  url: string,
+  data: any,
+  config?: AxiosRequestConfig
+): ApiResponse<T> => {
+  return axios.post<T>(api + url, data, config);
 };
 
 /**
  * Used to call PUT HTTP requests
  */
-export const putAPI = <T>(url: string, data: any, config?: AxiosRequestConfig): ApiResponse<T> => {
-  return axiosInstance.put<T>(url, data, config);
+export const putAPI = <T>(
+  url: string,
+  data: any,
+  config?: AxiosRequestConfig
+): ApiResponse<T> => {
+  return axios.put<T>(api + url, data, config);
 };
 
 /**
  * Used to call DELETE HTTP requests
  */
-export const deleteAPI = <T>(url: string, config?: AxiosRequestConfig): ApiResponse<T> => {
-  return axiosInstance.delete<T>(url, config);
+export const deleteAPI = <T>(
+  url: string,
+  config?: AxiosRequestConfig
+): ApiResponse<T> => {
+  return axios.delete<T>(api + url, config);
 };
 
 /**
