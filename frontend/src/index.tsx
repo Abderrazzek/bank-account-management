d21bb3d4bc355426cfd6391f0b71b757cd571565
YOUNSI <abderrazek.younsi@cgi.com>
2024-05-13 20:16:03 +0200

    :sparkles: add chakra ui provider for the app and use a theme for the app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AccountDetailContextProvider from "./accounts/details/context/AccountDetailContext";
+import theme from "assets/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AccountDetailContextProvider>
-      <App />
-    </AccountDetailContextProvider>
+    <ChakraProvider theme={theme}>
+      <AccountDetailContextProvider>
+        <App />
+      </AccountDetailContextProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
