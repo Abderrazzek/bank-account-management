1eaf85c81e8bf249c6c2e141d925d785547b4b83
YOUNSI <abderrazek.younsi@cgi.com>
2024-05-13 22:19:07 +0200

    :sparkles: add routing to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,27 @@ import React from "react";
 
 import SidebarWithHeader from "./components/sidebar";
 import AccountDetails from "layouts/accountDetails";
+import Dashboard from "layouts/dashboard";
+import Accounts from "layouts/accounts";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DeactivatedAccounts from "layouts/deactivatedAccounts";
+import FundTransfer from "layouts/fundTransfer";
 
 const App = () => {
   return (
     <div className="App">
       <SidebarWithHeader>
-        {/* <Accounts /> */}
-        <AccountDetails />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Dashboard} />
+            <Route path="/accounts" Component={Accounts} />
+            <Route path="/fund-transfer" Component={FundTransfer} />
+            <Route
+              path="/deactivated-accounts"
+              Component={DeactivatedAccounts}
+            />
+          </Routes>
+        </BrowserRouter>
       </SidebarWithHeader>
     </div>
   );
